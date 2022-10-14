9bfcc51cc75ffb85b8e7f428cfe2178a2b7f2cda
Shounak Das <112923983+Scienceloop@users.noreply.github.com>
2022-10-14 23:27:38 +0530

    Create Prime Number Checker.py

---
+++ Prime Number Checker.py
@@ -0,0 +1,12 @@
+def prime_checker(n):
+  is_prime= True
+  for N in range(2,n):
+    if n%N ==0:
+      is_prime= False
+  if is_prime:
+    print("It's a prime number.")
+  else:
+    print("It's not a prime number.")
+    
+n = int(input("Check this number: "))
+prime_checker(n)
